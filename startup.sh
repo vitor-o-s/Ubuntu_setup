ca0b3fa076fb02fee9c702bb2e4ba59610b51343
Vitor Oliveira Dos Santos <51249323+vitor-o-s@users.noreply.github.com>
2023-04-13 11:39:35 -0300

    Add PostgreSQL ao setup

--- startup.sh
+++ startup.sh
@@ -101,6 +101,9 @@ sudo add-apt-repository multiverse
 sudo apt update
 sudo apt install steam
 
+echo 'installing PostgreSQL'
+sudo apt install postgresql-14
+
 echo 'installing dbeaver'
 wget -c https://dbeaver.io/files/6.0.0/dbeaver-ce_6.0.0_amd64.deb
 sudo dpkg -i dbeaver-ce_6.0.0_amd64.deb
