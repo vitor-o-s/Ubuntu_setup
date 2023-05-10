d18733825def6e3a2cb57eb94d4458d8f6cab990
Vitor Oliveira Dos Santos <51249323+vitor-o-s@users.noreply.github.com>
2023-05-10 01:20:23 -0300

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -87,19 +87,8 @@ echo 'installing GNOME-Tweaks'
 sudo add-apt-repository universe
 sudo apt install gnome-tweak-tool
 
-echo 'installing Typora'
-sudo snap install typora
-
-echo 'installing VLC'
-sudo snap install vlc
-
-echo 'installing OBS Studio'
-sudo snap install obs-studio
-
-echo 'installing Steam'
-sudo add-apt-repository multiverse
-sudo apt update
-sudo apt install steam
+echo 'installing Postman'
+sudo snap install postman
 
 echo 'installing PostgreSQL'
 sudo apt install postgresql-14
@@ -123,6 +112,46 @@ sudo apt install pgadmin4-desktop
 sudo apt install pgadmin4-web 
 # Configure the webserver, if you installed pgadmin4-web:
 sudo /usr/pgadmin4/bin/setup-web.sh
+
+echo 'Installing Docker'
+# Update the apt package index and install packages to allow apt to use a repository over HTTPS:
+sudo apt-get update
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg
+#Add Docker’s official GPG key   
+sudo mkdir -m 0755 -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+# Use the following command to set up the repository:
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  
+#  Install Docker Engine
+#  Update the apt package index:
+sudo apt-get update
+# To install the latest version, run:
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+# Verify that the Docker Engine installation is successful by running the hello-world image:
+sudo docker run hello-world
+
+echo 'Okay, from here its just fun apps'
+
+echo 'installing Typora'
+sudo snap install typora
+
+echo 'installing VLC'
+sudo snap install vlc
+
+echo 'installing OBS Studio'
+sudo snap install obs-studio
+
+echo 'installing Steam'
+sudo add-apt-repository multiverse
+sudo apt update
+sudo apt install steam
 
 clear 
 
